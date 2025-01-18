27db9e69c16a1f5b99413887a9b4a5f45fcd6ac7
isurumihiranga234 <isurumihiranga234@gmail.com>
2025-01-18 07:56:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "HvRilTSQ#CdAJVba_i8xUz4k9B0e5NIRCSzlMX81Mvw8ejA4QLUQ", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-STATUES_REPLY: process.env.STATUES_REPLY || "fales",
+STATUES_REPLY: process.env.STATUES_REPLY || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
